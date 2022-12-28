2575df3e7b0c38da7953fae90beb52dc8037806e
Nitish Sharma <nitishsharma1503@gmail.com>
2022-12-28 20:11:17 +0530

    Add ID field to Job

--- src/main/java/com/connect/jobboard/domain/Job.java
+++ src/main/java/com/connect/jobboard/domain/Job.java
@@ -1,12 +1,20 @@
 package com.connect.jobboard.domain;
 
+import java.util.UUID;
+
 public class Job {
+    private final UUID id;
     private final String title;
     private final String location;
 
     public Job(String title, String location) {
         this.title = title;
         this.location = location;
+        id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public String getTitle() {
